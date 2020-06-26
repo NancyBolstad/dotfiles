349b115ddf7f869e3da6b6462824c8f172132b8f
NancyBolstad <litchacha@gmail.com>
2020-06-26 14:56:34 +0200

    rebase

---
+++ github/push.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+read -p "push or rebase: " oper
+if [ $oper == "push" ]
+then
+  git add -A
+  git status
+  read -p "Check that the files changes are correct. [y/n]: " variable
+  if [ $variable == "y" ]
+  then
+    git commit -m ":memo: Add some documents `date`"
+    git pull origin master
+    git push origin master
+  else
+    echo "It's not pushed"
+  fi
+else
+  git add -A
+  git commit -m "rebase"
+  git rebase -i HEAD~2
+  read -p "Modified to complete. [y/n]: " variable
+  if [ $variable == "y" ]
+  then
+    git push origin master -f
+  else
+    echo "It's not rebased"
+  fi
+fi
