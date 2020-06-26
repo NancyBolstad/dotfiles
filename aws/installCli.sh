e70027f63d9dd009336d539bf4d4f5c4a6d4bf84
NancyBolstad <litchacha@gmail.com>
2020-06-26 16:03:27 +0200

    :memo: Add some documents Fri Jun 26 16:03:27 CEST 2020

--- aws/installCli.sh
+++ aws/installCli.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-  if type aws > /dev/null; then
+if type aws > /dev/null; then
     echo "AWS cli already installed"
     return
-  fi
+fi
 
-  echo "Installing AWS cli..."
-  curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-  sudo installer -pkg AWSCLIV2.pkg -target /
-  echo "Completed AWS cli install"
+echo "Installing AWS cli..."
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+echo "Completed AWS cli install"
