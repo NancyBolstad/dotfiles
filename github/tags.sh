ad53a654618fa289d0e2aeadeb5946c233b1002b
NancyBolstad <litchacha@gmail.com>
2020-06-26 19:52:46 +0200

    Added git auto tag

--- github/tags.sh
+++ github/tags.sh
@@ -1,14 +1,13 @@
 tag="$(git describe --tags --always)"
 
-if [ -n "$(echo $tag | grep -E '.*\..*\..*')" ]
-then
-    major=$(echo $tag | cut -d. -f1)
-    minor=$(echo $tag | cut -d. -f2)
-    patch=$(echo $tag | cut -d. -f3)
-    major_version_tag=$major.x
-    minor_version_tag=$major.$minor.x
-    patch_version_tag=$major.$minor.$patch
-    tag_list="$major_version_tag $minor_version_tag $patch_version_tag"
+if [ -n "$(echo $tag | grep -E '.*\..*\..*')" ]; then
+  major=$(echo $tag | cut -d. -f1)
+  minor=$(echo $tag | cut -d. -f2)
+  patch=$(echo $tag | cut -d. -f3)
+  major_version_tag=$major.x
+  minor_version_tag=$major.$minor.x
+  patch_version_tag=$major.$minor.$patch
+  tag_list="$major_version_tag $minor_version_tag $patch_version_tag"
 else
-    tag_list=$tag
-    fi
+  tag_list=$tag
+fi
